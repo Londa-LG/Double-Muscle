ba8ce55c26a09f1630856e4b940e344b3eb1618a
Londa Mngadi <mngadilg@gmail.com>
2026-09-07 17:40:31 +0200

    init

---
+++ src/src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
+export default function App() {
+    return <MantineProvider>{/* Your app here */}</MantineProvider>;
+}
+
+export default App
