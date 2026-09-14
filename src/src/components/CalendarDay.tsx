1902c9e7a058f89e6e6d6f210b67f576b4320a11
Londa Mngadi <mngadilg@gmail.com>
2026-09-14 19:14:56 +0200

    Continued work on Workout calendar component

--- src/src/components/CalendarDay.tsx
+++ src/src/components/CalendarDay.tsx
@@ -1,11 +1,20 @@
 import { Paper,Flex,Text } from '@mantine/core';
 
-function CalendarDay(props:{ day: number })
+function CalendarDay(props:{ workout: bool, day: number })
 {
+  if(props.workout){
+    return(
+      <Paper bg="yellow" withBorder p="md">
+        <Flex algin="center" justify="center">
+          <Text c="white" size="xl">{ props.day }</Text>
+        </Flex>
+      </Paper>
+    );
+  }
   return(
-    <Paper withBorder p="md">
+    <Paper  withBorder p="md">
       <Flex algin="center" justify="center">
-        <Text size="xl">{ props.day }</Text>
+        <Text c="white" size="xl">{ props.day }</Text>
       </Flex>
     </Paper>
   );
--- src/src/components/WorkoutCalendar.tsx
+++ src/src/components/WorkoutCalendar.tsx
@@ -1,57 +1,42 @@
-import { Paper,Flex,Space } from '@mantine/core';
+import { Text,Badge ,Grid,Paper,Flex,Space } from '@mantine/core';
 import CalendarDay from './CalendarDay';
 
 function WorkoutCalendar()
 {
   return(
     <Paper withBorder p="lg">
-      <Flex justify="center" algin="center" direction="column" wrap="nowrap">
-        <Flex gap="lg" algin="center" wrap="nowrap" >
-          <CalendarDay day={1} />
-          <CalendarDay day={2} />
-          <CalendarDay day={3} />
-          <CalendarDay day={4} />
-          <CalendarDay day={5} />
-          <CalendarDay day={6} />
-          <CalendarDay day={7} />
-        </Flex>
-        <Space h="lg" />
-        <Flex gap="lg" algin="center" wrap="nowrap" >
-          <CalendarDay day={8} />
-          <CalendarDay day={9} />
-          <CalendarDay day={10} />
-          <CalendarDay day={11} />
-          <CalendarDay day={12} />
-          <CalendarDay day={13} />
-          <CalendarDay day={14} />
-        </Flex>
-        <Space h="lg" />
-        <Flex gap="lg" algin="center" wrap="nowrap" >
-          <CalendarDay day={15} />
-          <CalendarDay day={16} />
-          <CalendarDay day={17} />
-          <CalendarDay day={18} />
-          <CalendarDay day={19} />
-          <CalendarDay day={20} />
-          <CalendarDay day={21} />
-        </Flex>
-        <Space h="lg" />
-        <Flex gap="lg" algin="center" wrap="nowrap" >
-          <CalendarDay day={22} />
-          <CalendarDay day={23} />
-          <CalendarDay day={24} />
-          <CalendarDay day={25} />
-          <CalendarDay day={26} />
-          <CalendarDay day={27} />
-          <CalendarDay day={28} />
-        </Flex>
-        <Space h="lg" />
-        <Flex gap="lg" algin="center" wrap="nowrap" >
-          <CalendarDay day={29} />
-          <CalendarDay day={30} />
-          <CalendarDay day={31} />
-        </Flex>
-      </Flex>
+      <Badge variant="default" size="xl" radius="md">
+        September 2026
+      </Badge>
+      <Space h="xl" />
+      <Grid>
+        <Grid.Col span={1.7}>
+          <Text>
+            M
+          </Text>
+        </Grid.Col>
+        <Grid.Col span={1.7}>
+          <CalendarDay workout={true} day={1} />
+        </Grid.Col>
+        <Grid.Col span={1.7}>
+          <CalendarDay workout={false} day={2} />
+        </Grid.Col>
+        <Grid.Col span={1.7}>
+          <CalendarDay workout={false} day={3} />
+        </Grid.Col>
+        <Grid.Col span={1.7}>
+          <CalendarDay workout={true} day={4} />
+        </Grid.Col>
+        <Grid.Col span={1.7}>
+          <CalendarDay workout={false} day={5} />
+        </Grid.Col>
+        <Grid.Col span={1.7}>
+          <CalendarDay workout={false} day={6} />
+        </Grid.Col>
+        <Grid.Col span={1.7}>
+          <CalendarDay workout={false} day={7} />
+        </Grid.Col>
+      </Grid>
     </Paper>
   );
 }
